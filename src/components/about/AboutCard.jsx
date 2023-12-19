4feacc94066b619ff43da0f54c2810da81f9f827
rayhanyeager <86096057+rayhanyeager@users.noreply.github.com>
2023-12-19 12:59:44 +0700

    update

--- src/components/about/AboutCard.jsx
+++ src/components/about/AboutCard.jsx
@@ -1,28 +1,28 @@
-import React from "react"
-import Heading from "../common/heading/Heading"
-import "./about.css"
-import { homeAbout } from "../../dummydata"
-import Awrapper from "./Awrapper"
+import React from "react";
+import Heading from "../common/heading/Heading";
+import "./about.css";
+import { homeAbout } from "../../dummydata";
+import Awrapper from "./Awrapper";
 
 const AboutCard = () => {
   return (
     <>
-      <section className='aboutHome'>
-        <div className='container flexSB'>
-          <div className='left row'>
-            <img src='./images/logotim.png' alt='' />
+      <section className="aboutHome">
+        <div className="container flexSB">
+          <div className="left row">
+            <img src="./images/logotim.png" alt="" />
           </div>
-          <div className='right row'>
-            <Heading subtitle='' title='Futsal Club Omicron' />
-            <div className='items'>
+          <div className="right row">
+            <Heading subtitle="" title="Futsal Club Omicron" />
+            <div className="items">
               {homeAbout.map((val) => {
                 return (
-                  <div className='item flexSB'>
-                    <div className='text'>
+                  <div className="item flexSB">
+                    <div className="text">
                       <p>{val.desc}</p>
                     </div>
                   </div>
-                )
+                );
               })}
             </div>
           </div>
@@ -30,7 +30,7 @@ const AboutCard = () => {
       </section>
       <Awrapper />
     </>
-  )
-}
+  );
+};
 
-export default AboutCard
+export default AboutCard;
--- src/dummydata.js
+++ src/dummydata.js
@@ -3,7 +3,7 @@ export const homeAbout = [
     id: 1,
     desc: "Futsal Club Omicron adalah sebuah klub futsal amatir yang berbasis di Cigedug, Garut, Jawa Barat, yang bermain di Liga 1 Futsal Garut, Jawa Barat. Didirikan pada tahun 2022, oleh sekumpulan pemuda yang mempunyai visi yang sama yaitu memajukan futsal di garut. seiring berjalannya waktu, klub ini berubah menjadi akademi yang menampung anak-anak yang ingin berlatih futsal baik pria maupun wanita. ",
   },
-]
+];
 
 export const team = [
   {
@@ -14,16 +14,23 @@ export const team = [
     ig: "https://www.instagram.com/emilhamzah79real/?hl=id",
   },
   {
+    cover: "./images/team/rayhan.jpg",
+    name: "Rayhan Tsaury",
+    work: "SEKRETARIS",
+    fb: "https://www.facebook.com/profile.php?id=100009526743140",
+    ig: "https://www.instagram.com/ryhnts_/?hl=id",
+  },
+  {
     cover: "./images/team/regi.jpg",
     name: "Regi Dzikri",
-    work: "SEKRETARIS UMUM",
+    work: "WAKIL SEKRETARIS I",
     fb: "https://www.facebook.com/regi.regi.31586",
     ig: "https://www.instagram.com/regidikrt/?hl=id",
   },
   {
     cover: "./images/team/wandy.jpg",
     name: "Wandi Abdurrohman",
-    work: "WAKIL SEKRETARIS",
+    work: "WAKIL SEKRETARIS II",
     fb: "https://www.facebook.com/profile.php?id=100076934315147",
     ig: "https://www.instagram.com/wandirohim/?hl=id",
   },
@@ -42,13 +49,6 @@ export const team = [
     ig: "https://www.instagram.com/firdauscrown/?hl=id",
   },
   {
-    cover: "./images/team/rayhan.jpg",
-    name: "Rayhan Tsaury",
-    work: "ADMIN DAN EDITOR",
-    fb: "https://www.facebook.com/profile.php?id=100009526743140",
-    ig: "https://www.instagram.com/ryhnts_/?hl=id",
-  },
-  {
     cover: "./images/team/rizal.jpg",
     name: "Rizal Jalaludin",
     work: "PELATIH KEPALA",
@@ -56,15 +56,8 @@ export const team = [
     ig: "https://www.instagram.com/rizal.j_29/?hl=id",
   },
   {
-    cover: "./images/team/sansan.jpg",
-    name: "Sansan Zensonia",
-    work: "ASISTEN PELATIH",
-    fb: "https://www.facebook.com/profile.php?id=100084706804997",
-    ig: "https://www.instagram.com/sansanzensonia/?hl=id",
-  },
-  {
     cover: "./images/team/wendy.jpg",
-    name: "Wendy Seniata",
+    name: "Wendy Setiana",
     work: "PELATIH KIPER",
     fb: "https://www.facebook.com/wendy.setiana.9",
     ig: "https://www.instagram.com/wendysetiana2/?hl=id",
@@ -76,141 +69,4 @@ export const team = [
     fb: "https://www.facebook.com/profile.php?id=100038342084416",
     ig: "https://www.instagram.com/maulanarizky6931/?hl=id",
   },
-]
-
-export const store = [
-  {
-    cover: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIIpFliaO-pq-v_snQSPx0JYmNs8LAlMjS07Pu3rb2YA&s",
-    name: "Madrid kalah sama Mu",
-    price: "Rp.1.000.000",
-    link : "https://shopee.co.id/Jersey-Real-Madrid-2022-2023-3rd-third-baju-bola-olahraga-new-terbaru-22-23-grade-ori-import-High-Quality-22-23-i.14648614.21436833678?sp_atk=b83506c9-3334-437f-8931-b6660d6f4c1c&xptdk=b83506c9-3334-437f-8931-b6660d6f4c1c",
-  },
-]
-
-export const price = [
-  {
-    name: "BASIC PLAN",
-    price: "49K",
-    desc: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
-  },
-  {
-    name: "BEGINNER PLAN",
-    price: "79K",
-    desc: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
-  },
-  {
-    name: "PREMIUM PLAN",
-    price: "109k",
-    desc: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
-  },
-  {
-    name: "ULTIMATE PLAN",
-    price: "149K",
-    desc: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
-  },
-]
-export const faq = [
-  {
-    title: "How to Enroll This Online Courses?",
-    desc: "This is the first item's accordion body. It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow.",
-  },
-  {
-    title: "Where It is hidden by default, until the collapse?",
-    desc: "It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow.",
-  },
-  {
-    title: "How It is hidden by default, until the collapse?",
-    desc: "It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow.",
-  },
-  {
-    title: "How to Enroll This Online Courses?",
-    desc: "This is the first item's accordion body. It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow.",
-  },
-  {
-    title: "Where It is hidden by default, until the collapse?",
-    desc: "It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow.",
-  },
-  {
-    title: "How It is hidden by default, until the collapse?",
-    desc: "It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow.",
-  },
-]
-export const blog = [
-  {
-    id: 1,
-    type: "admin",
-    date: "JAN. 18, 2021",
-    com: "3 COMMENTS ",
-    title: "Build your Dream Software & Engineering Career",
-    desc: "A small river named Duden flows by their place and supplies it with the necessary regelialia.",
-    cover: "../images/blog/b1.webp",
-  },
-  {
-    id: 2,
-    type: "admin",
-    date: "API. 25, 2022",
-    com: "5 COMMENTS ",
-    title: "Build your Dream Software & Engineering Career",
-    desc: "A small river named Duden flows by their place and supplies it with the necessary regelialia.",
-    cover: "../images/blog/b2.webp",
-  },
-  {
-    id: 3,
-    type: "user",
-    date: "MAY. 15, 2022",
-    com: "10 COMMENTS ",
-    title: "Build your Dream Software & Engineering Career",
-    desc: "A small river named Duden flows by their place and supplies it with the necessary regelialia.",
-    cover: "../images/blog/b3.webp",
-  },
-  {
-    id: 4,
-    type: "admin",
-    date: "JAN. 02, 2022",
-    com: "3 COMMENTS ",
-    title: "Build your Dream Software & Engineering Career",
-    desc: "A small river named Duden flows by their place and supplies it with the necessary regelialia.",
-    cover: "../images/blog/b4.webp",
-  },
-  {
-    id: 5,
-    type: "admin",
-    date: "DEC. 14, 2022",
-    com: "3 COMMENTS ",
-    title: "Build your Dream Software & Engineering Career",
-    desc: "A small river named Duden flows by their place and supplies it with the necessary regelialia.",
-    cover: "../images/blog/b5.webp",
-  },
-  {
-    id: 6,
-    type: "user",
-    date: "JAN. 18, 2021",
-    com: "12 COMMENTS ",
-    title: "Build your Dream Software & Engineering Career",
-    desc: "A small river named Duden flows by their place and supplies it with the necessary regelialia.",
-    cover: "../images/blog/b6.webp",
-  },
-]
-export const testimonal = [
-  {
-    id: 1,
-    name: "ROGER SCOTT",
-    post: "MARKETING MANAGER",
-    desc: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
-    cover: "./images/testo/t1.webp",
-  },
-  {
-    id: 2,
-    name: "ROGER SCOTT",
-    post: "MARKETING MANAGER",
-    desc: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
-    cover: "./images/testo/t2.webp",
-  },
-  {
-    id: 3,
-    name: "ROGER SCOTT",
-    post: "MARKETING MANAGER",
-    desc: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
-    cover: "./images/testo/t3.webp",
-  },
-]
+];
